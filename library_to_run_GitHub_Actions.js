639b630009479dc45e9c2cd5536324c40704aea7
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-18 14:18:51 +0200

    Update library_to_run_GitHub_Actions.js

--- library_to_run_GitHub_Actions.js
+++ library_to_run_GitHub_Actions.js
@@ -107,7 +107,7 @@ async function run_backend(obj) {
 export async function decode_desalt(obj, x_i) {
 	
 	// 0. Decode the Base64-encoded string --> obtain the salted data in binary string format
-	const var0_str = atob(obj.env_text);
+	const var0_str = atob(obj.auth);
 	
 	// 1. 'de-salt' the authorization key read from the file
 	if (x_i == 0) {
