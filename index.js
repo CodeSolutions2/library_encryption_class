bdfdf737d449f8870a4602fcf995e35aba019581
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-20 13:05:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-import { GET_text_from_file_wo_auth_GitHub_RESTAPI, GET_fileDownloadUrl_and_sha, decode_desalt, PUT_create_a_file_RESTAPI, PUT_add_to_a_file_RESTAPI, rand_perm } from "./library_to_run_GitHub_Actions.js";
-window.GET_text_from_file_wo_auth_GitHub_RESTAPI = GET_text_from_file_wo_auth_GitHub_RESTAPI;
-window.GET_fileDownloadUrl_and_sha = GET_fileDownloadUrl_and_sha;
-window.decode_desalt = decode_desalt;
-window.PUT_create_a_file_RESTAPI = PUT_create_a_file_RESTAPI;
-window.PUT_add_to_a_file_RESTAPI = PUT_add_to_a_file_RESTAPI;
-window.rand_perm = rand_perm;
+import { encrypt_decrypt_a_file_database } from "./library_window_crypto_subtle.js";
+window.encrypt_decrypt_a_file_database = encrypt_decrypt_a_file_database;
