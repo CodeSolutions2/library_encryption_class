5c5ad63ec92ef6dd1e4b32a316100024d37b3b7e
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-06-20 14:02:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import { encrypt_decrypt_a_file_database } from "./library_window_crypto_subtle.js";
-window.encrypt_decrypt_a_file_database = encrypt_decrypt_a_file_database;
+import { add_username_to_file_database, view_file_database } from "./library_window_crypto_subtle.js";
+window.add_username_to_file_database = add_username_to_file_database;
+window.view_file_database = view_file_database;
