04ce4cd38c0c555c01ce018556bae527796ee261
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-07-04 14:19:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-import { encrypted_CRUD_file_database } from "./library_window_crypto_subtle_class.js";
-window.encrypted_CRUD_file_database = encrypted_CRUD_file_database;
+import { encrypted_CRUD_file_storage } from "./library_encryption_class.js";
+window.encrypted_CRUD_file_storage = encrypted_CRUD_file_storage;
+
+import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
+window.run_backend_process = run_backend_process;
