5fbe5e94337be7eb34432932523d65bafbde98af
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-07-04 14:50:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { encrypted_CRUD_file_storage } from "./library_encryption_class.js";
 window.encrypted_CRUD_file_storage = encrypted_CRUD_file_storage;
 
-// Uncaught SyntaxError: ambiguous indirect export: run_backend_process
-// import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
-// window.run_backend_process = run_backend_process;
+import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
+window.run_backend_process = run_backend_process;
