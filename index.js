21bc06cb56488e4674db68051ec562964af2ad03
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-27 18:50:31 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import { encrypted_CRUD_file_storage } from "./library_encryption_class.js";
-window.encrypted_CRUD_file_storage = encrypted_CRUD_file_storage;
-
-import { run_backend_process } from "./library_to_run_GitHub_Actions.js";
-window.run_backend_process = run_backend_process;
